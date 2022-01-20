8dc8b87650aa0145d02bb723f563d206e2e38990
m36 <m36@users.noreply.github.com>
2022-01-20 19:52:12 +0100

    1

---
+++ AntiVar.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Anti Varion
+// @namespace    http://varion.abschaffen/
+// @version      0.1
+// @description  Less fun is more fun!
+// @author       m3
+// @match        https://www.youtube.com/embed/*
+// @require      http://code.jquery.com/jquery-3.5.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var waitForEl = function(selector, callback) {
+        if (jQuery(selector).length) {
+            callback();
+        } else {
+            setTimeout(function() {
+                waitForEl(selector, callback);
+            }, 300);
+        }
+    };
+    waitForEl(".ytp-title-channel-logo", function() {
+        var blocklist = [
+            'UCw81xLtFkpks4D8_hYUP9VA', // Varion
+            'UCxm1sQQ53BGzKnhNZ9lZvCA', // Eigenwerbung von Kif
+            'UCh9IfI45mmk59eDvSWtuuhQ', // Varion-Kopie
+            'UCDyqjn-9rKdcAe0gTMAkqyg', // Noch eine Varion-Kopie
+            'UCy0Pr5u-MwGXXzp_GDd4m_g', // weibliche Varion-Kopie
+            'UCnBhY8jMEZzSSuFPz6JSAxQ' // Unlustiger Typ mit Behinderung
+        ];
+        $('.ytp-title-channel-logo').each(function () {
+            if ($.inArray($(this).attr('href').replace('https://www.youtube.com/channel/',''), blocklist) >= 0) {
+                $('body').html('Blocked by Anti-Varion extension');
+            }
+        });
+    });
+})();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Anti-Varion
- 
+für die Google Chrome Erweiterung "Tampermonkey".
+Installation:
+- Tampermonkey als Erweiterung hinzufügen: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
+- Link aufrufen: https://raw.githubusercontent.com/m36/anti-varion/master/AntiVar.user.js
+
+Wenn Kanäle fehlen einfach das Ende des Youtubelinks des entsprechenden Kanals kopieren und ab damit in die Blocklist.
